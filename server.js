3b31cc09f303d1c5ceac57db7e1ba8095db2a92d
siry siu <144174351+sirysiu@users.noreply.github.com>
2024-01-10 23:36:10 -0500

    set up the api route js

--- server.js
+++ server.js
@@ -8,12 +8,12 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Setup global notes variable
+//  global notes variable
 fs.readFile("./db/db.json","json",function(data) {
     var notes = data;
 });
 
-//Require routes file
+//routes file
 require('./routes/htmlRoutes')(app);
 require('./routes/apiRoutes')(app);
 
