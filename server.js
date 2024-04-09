834fdc7a8502d01fbf4d6360ac0b163b7f3557ce
siry siu <144174351+sirysiu@users.noreply.github.com>
2024-04-09 14:56:02 -0400

    test

--- server.js
+++ server.js
@@ -1,80 +1,23 @@
 const express = require("express");
-const path = require("path");
 const fs = require("fs");
 
-const db = require("./db/db.json")
-
+// Initialize express app
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.use(express.static('public'));
-
-
-app.get("/notes", (req, res) => res.sendFile(path.join(__dirname, "/public/notes.html")))
-
-app.get("/api/notes", (req, res) => {
-    res.status(200).json(db);
-})
-
-app.post("/api/notes", (req, res) => {
-    console.info(`${req.method} request received to add a note`);
-
-    // Deconstructing assignment for the items in req.body
-    const { title, text } = req.body;
-
-    // Checks if all data is present at submit. 
-    if (title && text) {
-
-        fs.readFile("./db/db.json", "utf8", (err, data) => {
-            if (err) {
-                console.error(err);
-                res.status(500).json('Error in posting note');
-                return;
-            }
-
-            const existingData = JSON.parse(data);
-
-            const newNote = {
-                title,
-                text,
-                // note_id: uuid(),
-            };
-
-            existingData.push(newNote);
-
-            const updatedDataString = JSON.stringify(existingData);
-
-            fs.writeFile("./db/db.json",
-                updatedDataString, (err) =>
-                err
-                    ? console.error(err)
-                    : console.log(
-                        `Note has been written to JSON file`
-                    )
-            );
-
-
-            const response = {
-                status: "success",
-                body: newNote,
-            };
-
-            console.log(response);
-            res.status(201).json(response);
-        });
-    } else {
-        res.status(500).json('Error in posting note');
-    }
-
-
-
+//  global notes variable
+fs.readFile("./db/db.json",function(data) {
+    var notes = data;
 });
 
-app.get("*", (req, res) => res.sendFile(path.join(__dirname, "/public/index.html")))
+//routes file
+require('./routes/htmlRoutes')(app);
+require('./routes/apiRoutes')(app);
 
-app.listen(PORT, () =>
-    console.log(`Example app listening at http://localhost:${PORT}`)
-);
+// Setup listener
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+}); 
