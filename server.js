88d979ce135927e18d45b9d6c4b0c44ede44d04b
siry siu <144174351+sirysiu@users.noreply.github.com>
2024-04-09 15:19:42 -0400

    change static

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static(__dirname, "public"));
 
 //  global notes variable
 fs.readFile("./db/db.json",function(data) {
