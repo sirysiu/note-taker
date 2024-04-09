48147227c296129544d512f340f1c1d149679e38
siry siu <144174351+sirysiu@users.noreply.github.com>
2024-04-09 15:07:43 -0400

    added public files for styles to show up

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
 //  global notes variable
 fs.readFile("./db/db.json",function(data) {
