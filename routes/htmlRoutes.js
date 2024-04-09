fcc80ad180799a361e50e049aa34e755bb135b2a
siry siu <144174351+sirysiu@users.noreply.github.com>
2024-04-09 15:01:49 -0400

    updated api routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -7,12 +7,12 @@ module.exports = (app) => {
 
   // Creating routes
   // GET notes should return the notes.html file.
-  app.get('/notes', (req, res) => {
+  app.get('/notes.html', (req, res) => {
     res.sendFile(path.join(__dirname, '../views/notes.html'));
   });
 
   // GET * should return the index.html file.
-  app.get('*', (req, res) => {
+  app.get('/index.html', (req, res) => {
     res.sendFile(path.join(__dirname, '../views/index.html'));
   })
 };
